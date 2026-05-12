f22d3d80e4ced4f9c9819f9d6ea3d4e592178fde
Maxence Coipelet <maxence_coipelet@etu.u-bourgogne.fr>
2026-05-12 14:46:23 +0200

    Correction affichage plein ecran des mini-jeux

--- ONAUBE(OneNightAtUBE)/sub_viewport_container.gd
+++ ONAUBE(OneNightAtUBE)/sub_viewport_container.gd
@@ -2,15 +2,33 @@ extends SubViewportContainer
 
 @export var scene: PackedScene
 var mini_game_instance = null
+@onready var sub_viewport: SubViewport = $SubViewport
+
+func _ready() -> void:
+	mettre_en_plein_ecran()
+	get_viewport().size_changed.connect(mettre_en_plein_ecran)
 
 func open_minigame():
+	mettre_en_plein_ecran()
 	if mini_game_instance == null:
 		mini_game_instance = scene.instantiate()
-		$SubViewport.add_child(mini_game_instance)
+		sub_viewport.add_child(mini_game_instance)
 
 	_activate_only_this()
 	
 	
+
+func mettre_en_plein_ecran() -> void:
+	var taille_fenetre := get_viewport_rect().size
+	set_anchors_preset(Control.PRESET_FULL_RECT)
+	custom_minimum_size = Vector2.ZERO
+	offset_left = 0.0
+	offset_top = 0.0
+	offset_right = 0.0
+	offset_bottom = 0.0
+	size = taille_fenetre
+	stretch = true
+	sub_viewport.size = Vector2i(taille_fenetre)
 
 
 func _activate_only_this():
